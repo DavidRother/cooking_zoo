642fd16050d7a487f160231bc9e32404b1e5c8c5
David Rother <david@edv-drucksysteme.de>
2021-07-22 17:03:07 +0200

    yay

--- gym_cooking/misc/game/game_loop.py
+++ gym_cooking/misc/game/game_loop.py
@@ -15,22 +15,25 @@ from datetime import datetime
 
 
 class GamePlay(Game):
-    def __init__(self, filename, world, sim_agents):
-        Game.__init__(self, world, sim_agents, play=True)
-        self.filename = filename
+
+    def __init__(self, env, num_humans, ai_policies):
+        Game.__init__(self, env.world, env.sim_agents, play=True)
+        self.env = env
         self.save_dir = 'misc/game/screenshots'
-        self.store = {"actions": [], "tensor_observations": []}
+        self.store = {"actions": [], "tensor_obs": []}
+        self.num_humans = num_humans
+        self.ai_policies = ai_policies
+        assert len(ai_policies) == len(env.sim_agents) - num_humans
         if not os.path.exists(self.save_dir):
             os.makedirs(self.save_dir)
 
         # tally up all gridsquare types
         self.gridsquares = []
-        self.gridsquare_types = defaultdict(set) # {type: set of coordinates of that type}
+        self.gridsquare_types = defaultdict(set)  # {type: set of coordinates of that type}
         for name, gridsquares in self.world.objects.items():
             for gridsquare in gridsquares:
                 self.gridsquares.append(gridsquare)
                 self.gridsquare_types[name].add(gridsquare.location)
-
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -38,27 +41,50 @@ class GamePlay(Game):
         elif event.type == pygame.KEYDOWN:
             # Save current image
             if event.key == pygame.K_RETURN:
-                image_name = '{}_{}.png'.format(self.filename, datetime.now().strftime('%m-%d-%y_%H-%M-%S'))
+                image_name = '{}_{}.png'.format(self.env.filename, datetime.now().strftime('%m-%d-%y_%H-%M-%S'))
                 pygame.image.save(self.screen, '{}/{}'.format(self.save_dir, image_name))
                 print('just saved image {} to {}'.format(image_name, self.save_dir))
                 return
-            
-            # Switch current agent
-            if pygame.key.name(event.key) in "1234":
-                try:
-                    self.current_agent = self.sim_agents[int(pygame.key.name(event.key))-1]
-                except:
-                    pass
-                return
 
-            # Control current agent
-            x, y = self.current_agent.location
-            if event.key in KeyToTuple.keys():
-                action = KeyToTuple[event.key]
-                self.current_agent.action = action
-                interact(self.current_agent, self.world)
+            # Control current human agent
+            if event.key in KeyToTuple_human1 and self.num_humans > 0:
+                action_translation_dict = {0: (0, 0), 1: (1, 0), 2: (0, 1), 3: (-1, 0), 4: (0, -1)}
+                reverse_action_translation_dict = {(0, 0): 0, (1, 0): 1, (0, 1): 2, (-1, 0): 3, (0, -1): 4}
+                action = KeyToTuple_human1[event.key]
+                self.sim_agents[0].action = action
+                tensor_obs = self.get_tensor_representation()
+                for idx, agent in enumerate(self.sim_agents):
+                    if idx >= self.num_humans:
+                        agent = self.ai_policies[idx - self.num_humans].agent
+                        obs = self.ai_policies[idx - self.num_humans].action_state_builder(tensor_obs)
+                        ai_action, _, _ = agent.get_action(obs)
+                        self.sim_agents[idx].action = action_translation_dict[ai_action.item()]
+                for agent in self.sim_agents:
+                    interact(agent, self.world)
+
+                self.env.step()
+                self.store["actions"].append([reverse_action_translation_dict[agent.action] for agent in self.sim_agents])
+                self.store["tensor_obs"].append(tensor_obs)
+
+            # if event.key in KeyToTuple_human2 and self.num_humans > 1:
+            #     action = KeyToTuple_human2[event.key]
+            #     self.sim_agents[1].action = action
+            #     interact(self.sim_agents[1], self.world)
+    def get_tensor_representation(self):
+        tensor = np.zeros((self.world.width, self.world.height, len(GAME_OBJECTS)))
+        objects = {"Player": self.sim_agents}
+        objects.update(self.world.objects)
+        for idx, name in enumerate(GAME_OBJECTS):
+            try:
+                for obj in objects[name]:
+                    x, y = obj.location
+                    tensor[x, y, idx] += 1
+            except KeyError:
+                continue
+        return tensor
 
     def on_execute(self):
+
         if self.on_init() == False:
             self._running = False
 
--- gym_cooking/misc/game/utils.py
+++ gym_cooking/misc/game/utils.py
@@ -4,13 +4,28 @@ import pygame
 class Color:
     BLACK = (0, 0, 0)
     FLOOR = (245, 230, 210)  # light gray
-    COUNTER = (220, 170, 110)   # tan/gray
+    COUNTER = (220, 170, 110)  # tan/gray
     COUNTER_BORDER = (114, 93, 51)  # darker tan
     DELIVERY = (96, 96, 96)  # grey
 
+
 KeyToTuple = {
-    pygame.K_UP    : ( 0, -1),  #273
-    pygame.K_DOWN  : ( 0,  1),  #274
-    pygame.K_RIGHT : ( 1,  0),  #275
-    pygame.K_LEFT  : (-1,  0),  #276
+    pygame.K_UP: (0, -1),  # 273
+    pygame.K_DOWN: (0, 1),  # 274
+    pygame.K_RIGHT: (1, 0),  # 275
+    pygame.K_LEFT: (-1, 0),  # 276
+}
+
+KeyToTuple_human1 = {
+    pygame.K_UP: (0, -1),  # 273
+    pygame.K_DOWN: (0, 1),  # 274
+    pygame.K_RIGHT: (1, 0),  # 275
+    pygame.K_LEFT: (-1, 0),  # 276
+}
+
+KeyToTuple_human2 = {
+    pygame.K_w: (0, -1),
+    pygame.K_s: (0, 1),
+    pygame.K_d: (1, 0),
+    pygame.K_a: (-1, 0),
 }
