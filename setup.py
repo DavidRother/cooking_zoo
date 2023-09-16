2c15f12d30c68c30939529ce9f85aaacfafbded1
David Rother <david@edv-drucksysteme.de>
2023-09-16 15:59:25 +0200

    pettingzoo version bump to 1.24

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='cooking_zoo',
       install_requires=[
             'numpy',
             'pygame',
-            'pettingzoo>=1.23',
+            'pettingzoo>=1.24',
             'Pillow',
             'Gymnasium>=0.26'
       ]
